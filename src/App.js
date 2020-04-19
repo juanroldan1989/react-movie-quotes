918484e8da117c3a6c9846ca3e0e5e75b986667a
Juan Roldan <juanroldan1989@gmail.com>
2020-04-19 03:54:00 -0300

    Home page - SearchInput component added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import SearchInput from './components/SearchInput';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    query : 'something'
+  }
+
+  // 'data' searched by to comunicate along other components
+  searchQuery = (data) => {
+    console.log(data);
+
+    this.setState({ query: data })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="app container">
           <div className="jumbotron">
             <h1 className="display-3">Movie Quotes!</h1>
-            <p className="lead">Choose Life. Choose a job. Choose a career. Choose a family.</p>
+            <p className="lead">ReactJS application interacting with MovieQuotes API : )</p>
             <hr className="my-4"></hr>
-            <p>ReactJS application interacting with MovieQuotes API : )</p>
+            <SearchInput searchQuery={this.searchQuery} />
           </div>
+
+          {this.state.query}
         </div>
       </div>
     )
---
+++ src/components/SearchInput.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class SearchInput extends Component {
+
+  searchRef = React.createRef();
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // get search input value
+    const value = this.searchRef.current.value;
+
+    // send value to parent component to comunicate it over other components
+    this.props.searchQuery(value);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="row">
+          <div className="form-group col-md-8">
+            <input ref={this.searchRef} type="text" className="form-control form-control-lg" placeholder="there is no spoon ..." />
+          </div>
+          <div className="form-group col-md-4">
+            <input type="submit" className="btn btn-lg btn-danger btn-block" value="Search" />
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SearchInput;
