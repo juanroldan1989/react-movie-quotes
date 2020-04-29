ef59babb0cd13cc04aa4f8eeebdac577dea2710a
Juan Roldan <juanroldan1989@gmail.com>
2020-04-28 22:36:42 -0300

    Search button disabled/enabled based on search content

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -14,15 +14,22 @@ class SearchInput extends Component {
     this.props.searchQuery(value);
   }
 
+  checkSearchButton = (e) => {
+    let value = e.target.value;
+    let disabled = (value === "");
+    let button = document.querySelector('#searchButton');
+    button.disabled = disabled;
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <div className="row">
           <div className="form-group col-md-8">
-            <input ref={this.searchRef} type="text" className="form-control form-control-lg" placeholder="there is no spoon ..." />
+            <input onChange={this.checkSearchButton} ref={this.searchRef} type="text" className="form-control form-control-lg" placeholder="there is no spoon ..." />
           </div>
           <div className="form-group col-md-4">
-            <input type="submit" className="btn btn-lg btn-danger btn-block" value="Search" />
+            <input disabled id="searchButton" type="submit" className="btn btn-lg btn-danger btn-block" value="Search" />
           </div>
         </div>
       </form>
