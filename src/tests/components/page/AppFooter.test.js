a15585e34d8311406658b61d59127afa9d401fd1
Juan Roldan <juanroldan1989@gmail.com>
2021-10-01 19:50:25 -0300

    AppFooter component - tests completed

--- src/tests/components/page/AppFooter.test.js
+++ src/tests/components/page/AppFooter.test.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { render} from "@testing-library/react";
-import AppFooter from '../../components/page/AppFooter';
+import AppFooter from '../../../components/page/AppFooter';
 
 describe("Footer", () => {
 
-  it("should show div", () => {
+  const setup = () => {
     const { getByTestId } = render(<AppFooter/>);
+    return getByTestId;
+  }
+
+  it("should show div", () => {
+    const getByTestId = setup();
     const div = getByTestId("footer-div");
 
     expect(div).toBeTruthy;
   })
 
   it("should show link within div", () => {
-    const { getByTestId } = render(<AppFooter/>);
+    const getByTestId = setup();
     const div = getByTestId("footer-div");
     const link = getByTestId("footer-link");
 
@@ -20,6 +25,9 @@ describe("Footer", () => {
   })
 
   it("should show link with proper URL", () => {
+    const getByTestId = setup();
+    const link = getByTestId("footer-link");
 
+    expect(link).toContainHTML("juanroldan.com.ar");
   })
 });
