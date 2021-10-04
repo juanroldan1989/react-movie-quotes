88a1ce2104d11feff18e55bbed8245166a0750e6
Juan Roldan <juanroldan1989@gmail.com>
2021-10-04 10:58:10 -0300

    SearchResult component - tests refactored and improved

--- src/tests/components/search/SearchResult.test.js
+++ src/tests/components/search/SearchResult.test.js
@@ -3,15 +3,17 @@ import { render, fireEvent } from "@testing-library/react";
 import SearchResult from "../../../components/search/SearchResult";
 
 describe("SearchResult", () => {
+  const quoteContent = "Hello Mr. Anderson";
+  const quote = {
+    content: quoteContent,
+    year: "1999",
+    movie: "The Matrix",
+    rating: "10",
+    character: { name: "Agent Smith" },
+    actor: { name: "Hugo Weaving" }
+  }
+
   it("should show card with the proper information", () => {
-    const quote = {
-      content: "Hello Mr. Anderson",
-      year: "1999",
-      movie: "The Matrix",
-      rating: "10",
-      character: { name: "Agent Smith" },
-      actor: { name: "Hugo Weaving" }
-    }
     const { getByTestId } = render(<SearchResult quote={quote} />);
     const card = getByTestId("quote-card");
 
@@ -25,14 +27,6 @@ describe("SearchResult", () => {
 
   describe("when card content is less than 3 lines", () => {
     it("should not display `show more` link", () => {
-      const quote = {
-        content: "Hello Mr. Anderson",
-        year: "1999",
-        movie: "The Matrix",
-        rating: "10",
-        character: { name: "Agent Smith" },
-        actor: { name: "Hugo Weaving" }
-      }
       const { getByText } = render(<SearchResult quote={quote} />);
       const showMoreText = getByText("Show more");
 
@@ -42,18 +36,16 @@ describe("SearchResult", () => {
 
   describe("when card content is more than 3 lines", () => {
     it("should display `show more` link", () => {
-      const quote = {
-        content: "Every mammal on this planet instinctively develops a natural equilibrium with the surrounding environment but you humans do not. You move to an area and you multiply and multiply until every natural resource is consumed and the only way you can survive is to spread to another area. There is another organism on this planet that follows the same pattern. Do you know what it is? A virus. Human beings are a disease, a cancer of this planet. You're a plague and we are the cure. Every mammal on this planet instinctively develops a natural equilibrium with the surrounding environment but you humans do not. You move to an area and you multiply and multiply until every natural resource is consumed and the only way you can survive is to spread to another area. There is another organism on this planet that follows the same pattern. Do you know what it is? A virus. Human beings are a disease, a cancer of this planet. You're a plague and we are the cure.",
-        year: "1999",
-        movie: "The Matrix",
-        rating: "10",
-        character: { name: "Agent Smith" },
-        actor: { name: "Hugo Weaving" }
-      }
+      const quoteContent =
+        "Every mammal on this planet instinctively develops a natural" +
+          "equilibrium with the surrounding environment but you humans do not." +
+            "You move to an area and you multiply and multiply until every natural" +
+              "resource is consumed and the only way you can survive is to spread to another area." +
+                "There is another organism on this planet that follows the same pattern. Do you know what it is? A virus."
       const { getByTestId } = render(<SearchResult quote={quote} />);
       const card = getByTestId("quote-card");
 
       expect(card).toContainHTML("Show more");
-    })
+    });
   });
 });
